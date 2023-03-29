4080ad6b530575f308849e8421f3eae8d618a11e
TIAGO EZEQUIEL GLUSMAN <47201568@ort>
2023-03-29 09:05:34 -0300

    no anda

--- scripts.js
+++ scripts.js
@@ -1,8 +1,44 @@
 
 let agregarTODO = document.getElementById("agregarTODO");
+let arrayTask = ["snieg"];
 
-agregarTODO.addEventListener("click", function getValue(inputTask) {
-    inputTask = document.getElementById("inputTask").value;
+arrayTask.forEach(item => {
+
+    console.log(arrayTask[0])
+    let inputTask = document.getElementById("inputTask").value;
+
+    let divTask = document.createElement("div");
+
+    let divForm = document.createElement("div");
+
+    let div = document.createElement("div");
+
+    let checkbox = document.createElement("input");
+
+    var text = document.createElement("label");
+
+    divTask.id = "task";
+    divTask.className = "input-group mb-3";
+    divForm.className = "input-group-text";
+    checkbox.type = "checkbox";
+    checkbox.className = "form-check-input mt-0";
+    checkbox.ariaLabel = "Checkbox for following text input";
+    text.className = "p-3 bg-info bg-opacity-10 border border-info border-start-0 rounded-end text"
+    if (inputTask != "") {
+        text.innerHTML = inputTask;
+        divTask.appendChild(divForm);
+        divTask.appendChild(text);
+        divForm.appendChild(div);
+        div.appendChild(checkbox);
+        document.body.appendChild(divTask);
+        console.log(divTask);
+    }
+})
+
+agregarTODO.onclick = () => {
+    
+
+    let inputTask = document.getElementById("inputTask").value;
 
     let divTask = document.createElement("div");
 
@@ -21,7 +57,7 @@ agregarTODO.addEventListener("click", function getValue(inputTask) {
     checkbox.type = "checkbox";
     checkbox.className = "form-check-input mt-0";
     checkbox.ariaLabel = "Checkbox for following text input";
-    text.className = "p-3 bg-info bg-opacity-10 border border-info border-start-0 rounded-end"
+    text.className = "p-3 bg-info bg-opacity-10 border border-info border-start-0 rounded-end text"
     if (inputTask != "") {
         text.innerHTML = inputTask;
         divTask.appendChild(divForm);
@@ -30,6 +66,7 @@ agregarTODO.addEventListener("click", function getValue(inputTask) {
         div.appendChild(checkbox);
         document.body.appendChild(divTask);
         console.log(divTask);
+        arrayTask.push(divTask.value)
     }
     
     checkbox.addEventListener("click", function() {
@@ -41,6 +78,5 @@ agregarTODO.addEventListener("click", function getValue(inputTask) {
             seTocoElBoton = true;
         }
     })
-} )
-
+}  
 
