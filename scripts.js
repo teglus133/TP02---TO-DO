6b7b4afc0dd69e61214f33d710246b0a91535646
AGUSTIN FULCO <47069276@ort>
2023-03-29 11:55:56 -0300

    sigue sin funcionar

--- scripts.js
+++ scripts.js
@@ -1,82 +1,76 @@
 
 let agregarTODO = document.getElementById("agregarTODO");
-let arrayTask = ["snieg"];
+let arrayTask = [];
 
-arrayTask.forEach(item => {
+    agregarTODO.onclick = () => {
 
-    console.log(arrayTask[0])
-    let inputTask = document.getElementById("inputTask").value;
+        console.log(arrayTask);
+        let inputTask = document.getElementById("inputTask").value
 
-    let divTask = document.createElement("div");
-
-    let divForm = document.createElement("div");
-
-    let div = document.createElement("div");
-
-    let checkbox = document.createElement("input");
-
-    var text = document.createElement("label");
-
-    divTask.id = "task";
-    divTask.className = "input-group mb-3";
-    divForm.className = "input-group-text";
-    checkbox.type = "checkbox";
-    checkbox.className = "form-check-input mt-0";
-    checkbox.ariaLabel = "Checkbox for following text input";
-    text.className = "p-3 bg-info bg-opacity-10 border border-info border-start-0 rounded-end text"
-    if (inputTask != "") {
-        text.innerHTML = inputTask;
-        divTask.appendChild(divForm);
-        divTask.appendChild(text);
-        divForm.appendChild(div);
-        div.appendChild(checkbox);
-        document.body.appendChild(divTask);
-        console.log(divTask);
-    }
-})
-
-agregarTODO.onclick = () => {
+        let divTask = document.createElement("div");
     
-
-    let inputTask = document.getElementById("inputTask").value;
-
-    let divTask = document.createElement("div");
-
-    let divForm = document.createElement("div");
-
-    let div = document.createElement("div");
-
-    let checkbox = document.createElement("input");
-
-    var text = document.createElement("label");
-
-    var seTocoElBoton = true;
-    divTask.id = "task";
-    divTask.className = "input-group mb-3";
-    divForm.className = "input-group-text";
-    checkbox.type = "checkbox";
-    checkbox.className = "form-check-input mt-0";
-    checkbox.ariaLabel = "Checkbox for following text input";
-    text.className = "p-3 bg-info bg-opacity-10 border border-info border-start-0 rounded-end text"
-    if (inputTask != "") {
-        text.innerHTML = inputTask;
-        divTask.appendChild(divForm);
-        divTask.appendChild(text);
-        divForm.appendChild(div);
-        div.appendChild(checkbox);
-        document.body.appendChild(divTask);
-        console.log(divTask);
-        arrayTask.push(divTask.value)
-    }
+        let divForm = document.createElement("div");
     
-    checkbox.addEventListener("click", function() {
-        if(seTocoElBoton) {
-            text.style.textDecoration = "line-through";
-            seTocoElBoton = false;
-        } else {
-            text.style.textDecoration = "none";
-            seTocoElBoton = true;
+        let div = document.createElement("div");
+    
+        let checkbox = document.createElement("input");
+    
+        var text = document.createElement("label");
+    
+        var seTocoElBoton = true;
+        divTask.id = "task";
+        divTask.className = "input-group mb-3";
+        divForm.className = "input-group-text";
+        checkbox.type = "checkbox";
+        checkbox.className = "form-check-input mt-0";
+        checkbox.ariaLabel = "Checkbox for following text input";
+        text.className = "p-3 bg-info bg-opacity-10 border border-info border-start-0 rounded-end text"
+        if (inputTask != "") {
+            text.innerHTML = inputTask;
+            divTask.appendChild(divForm);
+            divTask.appendChild(text);
+            divForm.appendChild(div);
+            div.appendChild(checkbox);
+            arrayTask.push(inputTask);
         }
-    })
+        
+        checkbox.addEventListener("click", function() {
+            if(seTocoElBoton) {
+                text.style.textDecoration = "line-through";
+                seTocoElBoton = false;
+            } else {
+                text.style.textDecoration = "none";
+                seTocoElBoton = true;
+            }
+        })
+        arrayTask.forEach(item => {
+            let inputTask = item;
+        
+            let divTask = document.createElement("div");
+        
+            let divForm = document.createElement("div");
+        
+            let div = document.createElement("div");
+        
+            let checkbox = document.createElement("input");
+        
+            var text = document.createElement("label");
+        
+            divTask.id = "task";
+            divTask.className = "input-group mb-3";
+            divForm.className = "input-group-text";
+            checkbox.type = "checkbox";
+            checkbox.className = "form-check-input mt-0";
+            checkbox.ariaLabel = "Checkbox for following text input";
+            text.className = "p-3 bg-info bg-opacity-10 border border-info border-start-0 rounded-end text"
+            if (inputTask != "") {
+                text.innerHTML = item;
+                divTask.appendChild(divForm);
+                divTask.appendChild(text);
+                divForm.appendChild(div);
+                div.appendChild(checkbox);
+                document.body.appendChild(divTask);
+            }
+        })
 }  
 
